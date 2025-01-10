8e8c6c7cb808276df5ba8e7be5e693743e8e2686
Mpilo <gcabampilo@gmail.com>
2025-01-10 02:21:20 +0200

    fixed linting and created a function to handle profile image

--- app/src/main/java/com/glucode/about_you/engineers/views/EngineerProfileCard.kt
+++ app/src/main/java/com/glucode/about_you/engineers/views/EngineerProfileCard.kt
@@ -3,7 +3,9 @@ package com.glucode.about_you.engineers.views
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.cardview.widget.CardView
+import androidx.core.net.toUri
 import com.glucode.about_you.R
 import com.glucode.about_you.databinding.ViewEngineerProfileCardBinding
 
@@ -13,29 +15,52 @@ class EngineerProfileCard @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : CardView(context, attrs, defStyleAttr) {
 
+    //region Properties
     private val binding: ViewEngineerProfileCardBinding =
         ViewEngineerProfileCardBinding.inflate(LayoutInflater.from(context), this)
 
-    var name:String = ""
+    var name: String = ""
         set(value) {
             binding.name.text = value
             field = value
         }
 
-    var role:String = ""
+    var role: String = ""
         set(value) {
             binding.role.text = value
             field = value
         }
 
-
     val quickStats: QuickStatsCardView = binding.quickStats
+
+    var clickListener: OnClickListener? = null
+    //endregion
+
+
+    //region Helper Functions
+    fun setProfileImage(str: String?) {
+        if (!str.isNullOrEmpty()) {
+            binding.profileImage.setImageURI(str.toUri())
+        } else {
+            binding.profileImage.setImageDrawable(
+                AppCompatResources.getDrawable(
+                    context,
+                    R.drawable.ic_person
+                )
+            )
+        }
+    }
+    //endregion
+
 
     init {
         radius = resources.getDimension(R.dimen.corner_radius_normal)
         binding.profileImage.setOnClickListener {
-
+            clickListener?.onClick()
         }
     }
 
+  interface OnClickListener {
+        fun onClick()
+    }
 }
